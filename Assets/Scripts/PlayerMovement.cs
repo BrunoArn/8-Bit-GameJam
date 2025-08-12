91864e1cc4313b37b69e1bd3e90353665064aad4
Bruno Barreiro Bastos Arnaud Fonseca <bruno.arnaud.br@gmail.com>
2025-08-12 15:44:36 -0300

    add player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,41 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 1f;
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+
+    private void Awake()
     {
-        
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnEnable() => playerControls.Enable();
+    void OnDisable() => playerControls.Disable();
+    
+
+    private void Update() {
+        PlayerInput();
     }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Combat.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.deltaTime));
+    }
+
 }
