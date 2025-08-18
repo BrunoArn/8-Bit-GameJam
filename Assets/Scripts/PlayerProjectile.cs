22cf2c31baf28884f82ec478900d0bbaa4dfd1c0
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2025-08-18 13:05:57 -0300

    add projectile shrink

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -8,13 +8,15 @@ public class PlayerProjectile : MonoBehaviour
     public float projectileSpeed = 12f;
     [SerializeField] float projectileRange = 10f;
     [SerializeField] private GameObject particle;
+    [SerializeField] private float minScaleFactor = 0.5f;
     
-
     private Vector2 startPosition;
+    private Vector3 initialScale;
+
     void Start()
     {
         startPosition = transform.position;
-
+        initialScale = transform.localScale;
     }
 
     void Update()
@@ -41,16 +43,22 @@ public class PlayerProjectile : MonoBehaviour
 
      private void MoveProjectile()
     {
-        transform.transform.Translate(Vector3.right * Time.deltaTime * projectileSpeed);
+        transform.transform.Translate(projectileSpeed * Time.deltaTime * Vector3.right);
     }
 
     private void DetectProjectileDistance()
     {
-        if (Vector3.Distance(transform.position, startPosition) > projectileRange)
+        //rezise projectile with distance reference
+        float distance = Vector3.Distance(startPosition, transform.position);
+        float normalizeDistance = Mathf.Clamp01(distance / projectileRange);
+        float scaleFactor = Mathf.Lerp(1f, minScaleFactor, normalizeDistance);
+
+        transform.localScale = initialScale * scaleFactor; 
+
+        if (distance > projectileRange)
         {
             //Instantiate(particle, transform.position, quaternion.identity);
             Destroy(this.gameObject);
         }
     }
-
 }
